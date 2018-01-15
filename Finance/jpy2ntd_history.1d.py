dd795954f4601e4cb6b4ae2e91a10e66767aa7a8
wwwins <jacky.huang@isobar.com>
2018-01-15 16:40:45 +0800

    Added showing JPY/NTD rate history

---
+++ Finance/jpy2ntd_history.1d.py
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+SPARK="/usr/local/bin/spark"
+CURL="/usr/bin/curl"
+AWK="/usr/bin/awk"
+GREP="/usr/bin/grep"
+TAIL="/usr/bin/tail"
+HEAD="/usr/bin/head"
+URL="http://rate.bot.com.tw/xrt/flcsv/0/L3M/JPY"
+
+if [ ! -f "${SPARK}" ]; then
+    echo "Install spark utility please."
+    exit 1
+fi
+
+if [ ! -f "${CURL}" ]; then
+    echo "Install curl please."
+    exit 1
+fi
+
+${CURL} ${URL} --silent | ${GREP} JPY | ${AWK} -F ',' '{print $14*1000}' | ${HEAD} -n 60 | ${TAIL} -r | ${SPARK}
