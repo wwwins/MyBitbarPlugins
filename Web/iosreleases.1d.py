d4937a2124afc9090cbeed0888077469d4b190f8
wwwins <jacky.huang@isobar.com>
2018-01-15 16:50:17 +0800

    Minor update

--- Web/iosreleases.1d.py
+++ Web/iosreleases.1d.py
@@ -9,20 +9,24 @@
 
 from lxml import etree
 
-color = "cadetblue"
+color = "slateblue"
 ios_version = ""
 
 def format_text(xml):
-    return (u'%s | href=%s' % (xml[0].text, xml[5].text)).encode('utf-8')
+	return (u'%s | href=%s' % (xml[0].text, xml[5].text)).encode('utf-8')
 
 doc = etree.parse("http://twitrss.me/twitter_user_to_rss/?user=iOSReleases")
 root = doc.getroot()
 
 for child in root[0][6:]:
-    if "iOS" in child[0].text:
-        if not ios_version:
-            ios_version = child[0].text.split(' ')[1]
-            print(u'iOS:'+ios_version+"|color="+color).encode('utf-8')
-            print('---')
-        print format_text(child)
+	if "iOS" in child[0].text:
+		start = child[0].text.index('for ')
+		end = child[0].text.index(' device')
+		cnt_devices = int(child[0].text[start+4:end])
+		if cnt_devices>10:
+			if ios_version=="":
+				ios_version = child[0].text.split(' ')[1]
+				print(u'iOS:'+ios_version+"|color="+color).encode('utf-8')
+				print('---')
+			print format_text(child)
 print('---')
